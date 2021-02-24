f45cac90a0db14dfe64c6e9edf9eaf9d7d260087
Alexa Summers <alexas0202@gmail.com>
2021-02-24 02:03:55 -0600

    Add Handles
    Add Handle scripts for editing the scene

--- Assets/Editor/CubeEditor.cs
+++ Assets/Editor/CubeEditor.cs
@@ -3,36 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-// [CustomEditor(typeof(Cube))]
+[CustomEditor(typeof(Cube))]
 
 public class CubeEditor : Editor
 {
-    // public override void OnInspectorGUI()
-    // {
-    //     SerializedProperty cubeStatsSP = serializedObject.FindProperty("cube");
-    //     if (cubeStatsSP.objectReferenceValue == null)
-    //     {
-    //         EditorGUILayout.PropertyField(cubeStatsSP);
-    //     }
-    //     else
-    //     {
-    //         CubeStats cube = cubeStatsSP.objectReferenceValue as CubeStats;
+    public override void OnInspectorGUI()
+    {
+        SerializedProperty cubeStatsSP = serializedObject.FindProperty("cube");
+        if (cubeStatsSP.objectReferenceValue == null)
+        {
+            EditorGUILayout.PropertyField(cubeStatsSP);
+        }
+        else
+        {
+            CubeStats cube = cubeStatsSP.objectReferenceValue as CubeStats;
 
-    //         EditorGUILayout.LabelField(cube.displayName + " Stats");
-    //         EditorGUILayout.PropertyField(cubeStatsSP);
-    //         EditorGUILayout.Space();
-    //         cube.strength = EditorGUILayout.IntField("Strength: ", cube.strength);
-    //         cube.body = EditorGUILayout.IntField("Body: ", cube.body);
-    //         cube.will = EditorGUILayout.IntField("Will: ", cube.will);
-    //         EditorGUILayout.Space();
-    //         int health = ((cube.strength + cube.body + cube.will) / 3) * 4;
-    //         EditorGUILayout.LabelField("Calculated Stats: ");
-    //         EditorGUILayout.LabelField("Health: " + health);
-    //         EditorGUILayout.Space();
-    //         EditorGUILayout.LabelField("Stat: ");
-    //         SerializedProperty stat = serializedObject.FindProperty("stat");
-    //         stat.stringValue = EditorGUILayout.TextField("Stat ", stat.stringValue);
-    //     }
-    //      serializedObject.ApplyModifiedProperties();
-    // }
+            EditorGUILayout.LabelField(cube.displayName + " Stats");
+            EditorGUILayout.PropertyField(cubeStatsSP);
+            EditorGUILayout.Space();
+            cube.strength = EditorGUILayout.IntField("Strength: ", cube.strength);
+            cube.body = EditorGUILayout.IntField("Body: ", cube.body);
+            cube.will = EditorGUILayout.IntField("Will: ", cube.will);
+            EditorGUILayout.Space();
+            int health = ((cube.strength + cube.body + cube.will) / 3) * 4;
+            EditorGUILayout.LabelField("Calculated Stats: ");
+            EditorGUILayout.LabelField("Health: " + health);
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Stat: ");
+            SerializedProperty stat = serializedObject.FindProperty("stat");
+            stat.stringValue = EditorGUILayout.TextField("Stat ", stat.stringValue);
+        }
+         serializedObject.ApplyModifiedProperties();
+    }
 }
