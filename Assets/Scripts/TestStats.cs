f8b0e67bea4cf972f1fa0306306c251bc76bf419
Alexa Summers <alexas0202@gmail.com>
2021-04-07 19:05:09 -0500

    Add Enum example
    Enum example for UI lecture

--- Assets/Scripts/TestStats.cs
+++ Assets/Scripts/TestStats.cs
@@ -2,13 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 public class TestStats : MonoBehaviour
 {
  public int health;
  public int power;
  public int stamina;
+
+ public enum Enum {
+     option1,
+     option2,
+     option3,
+     option4,
+     option5,
+     option6,
+     option7,
+     option8,
+     option9,
+     option10,
+     option11,
+     option12,
+     option13,
+     option14,
+     option15,
+     option16,
+     option17,
+     option18,
+     option19,
+     option20,
+     option21,
+     option22,
+     option23,
+     option24,
+     option25,
+     option26,
+     option27,
+     option28,
+     option29
+ }
+
+ public Enum enum1;
+
 }
